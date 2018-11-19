fdae8a3e66a87f8691011dd8567487478cc75fe6
Matthew Sherman <msherman0801@gmail.com>
2018-11-19 22:30:37 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+require 'pry'
+class Anagram 
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    array.each do |a|
+      if a.split("").sort != @word.split("").sort
+        array.delete(a)
+      end
+      binding.pry
+    end
+    array
+  end
+end
